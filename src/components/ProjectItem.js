893d7b48e6e9b13c1b331576ec6f52fe42177573
Medetcan Diler <medetdiler1996@gmail.com>
2023-06-05 18:56:59 +0300

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech,i) => (
+          <span key={i}>{tech}</span>
+        ))}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,15 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log(projects, 'sdf');
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+        {projects.map(project => (
+          <ProjectItem key={project.id} technologies={project.technologies} name={project.name} about={project.about} />
+        ))}
+      </div>
     </div>
   );
 }
